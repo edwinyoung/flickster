e51c3a402e048d9e058a3cad3f458b1a694261ca
Ed Young <edwin.w.young@gmail.com>
2017-01-26 22:26:16 -0800

    Finished Part 2, confirmed pulling data from MovieDB API

--- Flickster/MoviesViewController.swift
+++ Flickster/MoviesViewController.swift
@@ -11,39 +11,67 @@ import UIKit
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     
-    @IBOutlet weak var tableView: UITableView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
-        tableView.dataSource = self
-        tableView.delegate = self
-    }
+	@IBOutlet weak var tableView: UITableView!
+	
+	var movies : [NSDictionary]?
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		// Do any additional setup after loading the view.
+		tableView.dataSource = self
+		tableView.delegate = self
+		
+		let apiKey = "8c196c3c0b660eff61aa8b14f87d402e"
+		let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
+		let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+		let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+		let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
+			if let data = data {
+				if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+					print(dataDictionary)
+					
+					self.movies = dataDictionary["results"] as! [NSDictionary]
+					self.tableView.reloadData()
+				}
+			}
+		}
+		task.resume()
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		if let movies = movies {
+			return movies.count
+		} else {
+			return 0
+		}
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+		
+		let movie = movies![indexPath.row]
+		let title = movie["title"] as! String
+		
+		
+		cell.textLabel!.text = title
+		print(title)
+		return cell
+	}
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
-        cell.textLabel!.text = "Row \(indexPath.row)"
-        print("Row \(indexPath.row)")
-        return cell
-    }
-    
-    /*
-     // MARK: - Navigation
+	/*
+		// MARK: - Navigation
      
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
+		// In a storyboard-based application, you will often want to do a little preparation before navigation
+		override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+			// Get the new view controller using segue.destinationViewController.
+			// Pass the selected object to the new view controller.
+		}
+	*/
     
 }
