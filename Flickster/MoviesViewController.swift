c4f563740de1c85b94e1e89f06ddb5034cf3e4a9
Ed Young <edwin.w.young@gmail.com>
2017-01-26 16:26:54 -0800

    Removed dead connection in Main.storyboard

--- Flickster/MoviesViewController.swift
+++ Flickster/MoviesViewController.swift
@@ -9,17 +9,17 @@
 import UIKit
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+    
     
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Do any additional setup after loading the view.
         tableView.dataSource = self
         tableView.delegate = self
-        // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -35,15 +35,15 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         print("Row \(indexPath.row)")
         return cell
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
